911f92a5fb74c73ceba65b7420e224c1b34e94eb
amulware <amul@amulware.net>
2013-05-25 19:06:06 +0200

    added font class got json converter base class from nodatime, thanks jon! started work on font converter

---
+++ src/serialization/jsonnet/FontConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace amulware.Graphics.Serialization.JsonNet
+{
+    /// <summary>
+    /// Json.NET converter for <see cref="Font"/>.
+    /// </summary>
+    internal sealed class FontConverter : JsonConverterBase<Font>
+    {
+        /// <summary>
+        /// Reads a string from the reader, and converts it to a font.
+        /// </summary>
+        /// <param name="reader">The JSON reader to fetch data from.</param>
+        /// <param name="serializer">The serializer for embedded serialization.</param>
+        /// <returns>The <see cref="Font"/> identified in the JSON, or null.</returns>
+        protected override Font readJsonImpl(JsonReader reader, JsonSerializer serializer)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Converts the given <see cref="Font"/> to JSON.
+        /// </summary>
+        /// <param name="writer">The writer to write to</param>
+        /// <param name="value">The value to convert</param>
+        /// <param name="serializer">Unused by this serializer</param>
+        protected override void writeJsonImpl(JsonWriter writer, Font value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException("Json serializing of type Font not yet implemented.");
+        }
+    }
+}
---
+++ src/utilities/Font.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace amulware.Graphics
+{
+    class Font
+    {
+        public Vector2 PixelSymbolOffset { get; private set; }
+        public Vector2 PixelSymbolSize { get; private set; }
+        public Vector2 SymbolSize { get; private set; }
+
+        public bool MonoSpaced { get; private set; }
+
+        private float[] letterWidths;
+
+        internal Font(
+            Vector2 pixelSymbolOffset,
+            Vector2 pixelSymbolSize,
+            Vector2 symbolSize,
+            float[] letterWidths = null
+            )
+        {
+            this.PixelSymbolOffset = pixelSymbolOffset;
+            this.PixelSymbolSize = pixelSymbolSize;
+            this.SymbolSize = symbolSize;
+            if (letterWidths != null &&
+                letterWidths.Length == 256)
+                this.letterWidths = letterWidths;
+            this.MonoSpaced = this.letterWidths == null;
+        }
+
+        public float LetterWidth(int c)
+        {
+            return this.letterWidths[c];
+        }
+}
+}
