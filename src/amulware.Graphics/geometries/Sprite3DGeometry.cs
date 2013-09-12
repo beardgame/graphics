cb8e0b9dd28493f42493d987c6e9acab5b1e055f
amulware <amul@amulware.net>
2013-09-12 16:56:03 +0200

    refactored sprite 3d geometry to be deriving from uvquadgeometry

--- src/amulware.Graphics/geometries/Sprite3DGeometry.cs
+++ src/amulware.Graphics/geometries/Sprite3DGeometry.cs
@@ -1,4 +1,4 @@
-﻿using OpenTK;
+using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
 namespace amulware.Graphics
@@ -6,38 +6,12 @@ namespace amulware.Graphics
     /// <summary>
     /// Geometry for drawing camera space aligned sprites in three dimensional space.
     /// </summary>
-    public class Sprite3DGeometry : Geometry<SimpleSpriteVertexData>
+    public class Sprite3DGeometry : UVQuadGeometry<SimpleSpriteVertexData>
     {
         /// <summary>
         /// The color used for drawing
         /// </summary>
         public Color Color = Color.White;
-
-        /// <summary>
-        /// The size to draw the sprite with
-        /// </summary>
-        public Vector2 Size
-        {
-            get
-            {
-                return this.size;
-            }
-            set
-            {
-                this.expandX = value.X * 0.5f;
-                this.expandY = value.Y * 0.5f;
-                this.size = value;
-            }
-        }
-
-        private Vector2 size = Vector2.One;
-        private float expandX = 0.5f;
-        private float expandY = 0.5f;
-
-        /// <summary>
-        /// The <see cref="UVRectangle"/> used to map the sprite with. Obtain from <see cref="Texture.GrabUV"/>
-        /// </summary>
-        public UVRectangle UV = UVRectangle.Default;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Sprite3DGeometry"/> class.
@@ -60,6 +34,33 @@ namespace amulware.Graphics
                 new SimpleSpriteVertexData(position, this.UV.BottomRight, this.Color, this.expandX, -this.expandY),
                 new SimpleSpriteVertexData(position, this.UV.BottomLeft, this.Color, -this.expandX, -this.expandY)
                 });
+        }
+
+        public override void DrawSprite(Vector3 position, float angle, float scale)
+        {
+            Vector2 expand = new Vector2(expandX, expandY) * scale;
+
+            if (angle != 0)
+            {
+                expand = Matrix2.CreateRotation(angle) * expand;
+            }
+
+            this.Surface.AddVertices(new SimpleSpriteVertexData[] {
+                new SimpleSpriteVertexData(position, this.UV.TopLeft, this.Color, -expand.X, expand.Y),
+                new SimpleSpriteVertexData(position, this.UV.TopRight, this.Color, expand.X, expand.Y),
+                new SimpleSpriteVertexData(position, this.UV.BottomRight, this.Color, expand.X, -expand.Y),
+                new SimpleSpriteVertexData(position, this.UV.BottomLeft, this.Color, -expand.X, -expand.Y)
+                });
+        }
+
+        public override void DrawRectangle(float x, float y, float z, float w, float h)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void DrawLine(float x1, float y1, float z1, float x2, float y2, float z2)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
