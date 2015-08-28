274bbc48f23879ab110d16f773b018774d9cf863
amulware <amul@amulware.net>
2015-08-28 19:43:00 +0200

    added mesh->surface creation, includiong vertex optional transform and support for point clouds, closes #2

--- src/amulware.Graphics/Meshes/Mesh.cs
+++ src/amulware.Graphics/Meshes/Mesh.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace amulware.Graphics.Meshes
 {
     sealed partial class Mesh
@@ -12,5 +13,72 @@ namespace amulware.Graphics.Meshes
             this.triangles = triangles;
         }
 
+        public IndexedSurface<MeshVertex> ToIndexedSurface(Func<MeshVertex, MeshVertex> transform = null)
+        {
+            var surface = new IndexedSurface<MeshVertex>();
+
+            this.writeVertices(surface, transform);
+            this.writeIndices(surface);
+
+            return surface;
+        }
+        public VertexSurface<MeshVertex> ToPointCloudSurface(Func<MeshVertex, MeshVertex> transform = null)
+        {
+            var surface = new VertexSurface<MeshVertex>();
+
+            this.writeVertices(surface, transform);
+
+            return surface;
+        }
+
+        private void writeIndices(IndexedSurface<MeshVertex> surface)
+        {
+            int iOffset;
+            var indexArray = surface.WriteIndicesDirectly(this.vertices.Length * 3, out iOffset);
+
+            if (iOffset != 0)
+            {
+                // test not strictly speaking necessary, but enforces usage
+                throw new Exception("Expected index offset to be zero.");
+            }
+
+            for (int i = 0; i < this.triangles.Length; i++)
+            {
+                var triangle = this.triangles[i];
+
+                indexArray[iOffset] = (ushort)triangle.Index0;
+                indexArray[iOffset + 1] = (ushort)triangle.Index1;
+                indexArray[iOffset + 2] = (ushort)triangle.Index2;
+
+                iOffset += 3;
+            }
+        }
+
+        private void writeVertices(
+            VertexSurface<MeshVertex> surface,
+            Func<MeshVertex, MeshVertex> transform = null
+            )
+        {
+            ushort vOffset;
+            var vertexArray = surface
+                .WriteVerticesDirectly(this.vertices.Length, out vOffset);
+
+            if (vOffset != 0)
+            {
+                throw new Exception("Expected vertex offset to be zero.");
+            }
+
+            if (transform == null)
+            {
+                this.vertices.CopyTo(vertexArray, 0);
+            }
+            else
+            {
+                for (int i = 0; i < this.vertices.Length; i++)
+                {
+                    vertexArray[i] = transform(this.vertices[i]);
+                }
+            }
+        }
     }
 }
