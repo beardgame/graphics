e06fc6a63aed44900d937abe9d943ab6c1cdb916
amulware <amul@amulware.net>
2019-03-19 19:28:38 +0000

    ✨ Add array texture uniform

---
+++ src/amulware.Graphics/Core/surfaces/settings/ArrayTextureUniform.cs
@@ -0,0 +1,27 @@
+using OpenTK.Graphics.OpenGL;
+
+namespace amulware.Graphics
+{
+    public class ArrayTextureUniform : SurfaceSetting
+    {
+        public string Name { get; }
+        
+        public ArrayTexture Texture { get; set; }
+
+        public TextureUnit Target { get; set; }
+
+        public ArrayTextureUniform(string name, ArrayTexture texture, TextureUnit target = TextureUnit.Texture0)
+        {
+            Name = name;
+            Texture = texture;
+            Target = target;
+        }
+
+        public override void Set(ShaderProgram program)
+        {
+            GL.ActiveTexture(Target);
+            GL.BindTexture(TextureTarget.Texture2DArray, Texture);
+            GL.Uniform1(program.GetUniformLocation(Name), Target - TextureUnit.Texture0);
+        }
+    }
+}
