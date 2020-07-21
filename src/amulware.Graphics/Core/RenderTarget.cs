0ca524bf187a6ccbad4f955416117d6d93fc54f6
amulware <amul@amulware.net>
2020-07-21 20:38:54 +0100

    🎯 Refactor render target

--- src/amulware.Graphics/Core/RenderTarget.cs
+++ src/amulware.Graphics/Core/RenderTarget.cs
@@ -1,112 +1,61 @@
 using System;
+using System.Linq;
 using OpenToolkit.Graphics.OpenGL;
 
 namespace amulware.Graphics
 {
-    /// <summary>
-    /// This class represents an OpenGL framebuffer object that can be rendered to.
-    /// </summary>
     public sealed class RenderTarget : IDisposable
     {
-        #region Fields
-
         private readonly int handle;
 
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// The handle of the OpenGL framebuffer object associated with this render target
-        /// </summary>
-        public int Handle => handle;
-
-        #endregion
-
-        #region Constructors
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RenderTarget"/> class.
-        /// </summary>
         public RenderTarget()
         {
             GL.GenFramebuffers(1, out handle);
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="T:amulware.Graphics.RenderTarget" /> class assigning a texture
-        /// to its default color attachment.
-        /// </summary>
-        /// <param name="texture">The texture to attach.</param>
-        public RenderTarget(Texture texture)
-            : this()
+        public Target Bind(FramebufferTarget target = FramebufferTarget.DrawFramebuffer)
         {
-            Attach(FramebufferAttachment.ColorAttachment0, texture);
+            return new Target(this, target);
         }
 
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Attaches a <see cref="Texture"/> to the specified <see cref="FramebufferAttachment"/>, so it can be rendered\
-        /// to.
-        /// </summary>
-        /// <param name="attachment">The attachment.</param>
-        /// <param name="texture">The texture.</param>
-        /// <param name="target">Texture target of the attachment.</param>
-        public void Attach(
-            FramebufferAttachment attachment,
-            Texture texture,
-            TextureTarget target = TextureTarget.Texture2D)
+        public readonly struct Target : IDisposable
         {
-            GL.BindFramebuffer(FramebufferTarget.DrawFramebuffer, handle);
-            GL.FramebufferTexture2D(FramebufferTarget.DrawFramebuffer, attachment, target, texture, 0);
-            GL.BindFramebuffer(FramebufferTarget.DrawFramebuffer, 0);
+            private readonly FramebufferTarget target;
+
+            internal Target(RenderTarget renderTarget, FramebufferTarget target)
+            {
+                this.target = target;
+                GL.BindFramebuffer(target, renderTarget.handle);
+            }
+
+            public void SetColorAttachments(params Texture[] textures)
+            {
+                var attachments = new DrawBuffersEnum[textures.Length];
+                foreach (var index in Enumerable.Range(0, textures.Length))
+                {
+                    Attach(FramebufferAttachment.ColorAttachment0 + index, textures[index]);
+                    attachments[index] = DrawBuffersEnum.ColorAttachment0 + index;
+                }
+                GL.DrawBuffers(attachments.Length, attachments);
+            }
+
+            public void Attach(
+                FramebufferAttachment attachment,
+                Texture texture,
+                TextureTarget textureTarget = TextureTarget.Texture2D)
+            {
+                GL.FramebufferTexture2D(target, attachment, textureTarget, texture.Handle, 0);
+            }
+
+            public void Dispose()
+            {
+                GL.BindFramebuffer(target, 0);
+            }
         }
 
-        #endregion
-
-        #region Operators
-
-        /// <summary>
-        /// Casts the <see cref="RenderTarget"/> to its OpenGL framebuffer object handle, for easy use with OpenGL
-        /// functions.
-        /// </summary>
-        /// <remarks>Null is cast to 0.</remarks>
-        public static implicit operator int(RenderTarget renderTarget) => renderTarget?.Handle ?? 0;
-
-        #endregion
-
-        #region Disposing
-
-        private bool disposed;
-
-        /// <summary>
-        /// Disposes of the render target and deletes the underlying GL object.
-        /// </summary>
         public void Dispose()
         {
-            dispose();
-            GC.SuppressFinalize(this);
+            GL.DeleteFramebuffer(handle);
         }
-
-        private void dispose()
-        {
-            if (disposed)
-                return;
-
-            var handleCopy = Handle;
-            GL.DeleteFramebuffers(1, ref handleCopy);
-
-            disposed = true;
-        }
-
-        ~RenderTarget()
-        {
-            dispose();
-        }
-
-        #endregion
     }
 }
