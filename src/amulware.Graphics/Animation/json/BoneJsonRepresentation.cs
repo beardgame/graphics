531b8c69409e4938052df8b1960f973cdff6258b
amulware <amul@amulware.net>
2020-07-21 20:56:27 +0100

    🔥🦴

--- src/amulware.Graphics/Animation/json/BoneJsonRepresentation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace amulware.Graphics.Animation
-{
-    class BoneJsonRepresentation<TBoneAttributes>
-    {
-        public string Name { get; set; }
-        public string Parent { get; set; }
-        public TBoneAttributes Attributes { get; set; }
-    }
-}
--- src/amulware.Graphics/Animation/json/KeyframeDataJsonRepresentation.cs
+++
@@ -1,8 +0,0 @@
-namespace amulware.Graphics.Animation
-{
-    internal class KeyframeDataJsonRepresentation<TKeyframeParameters>
-    {
-        public string Bone { get; set; }
-        public TKeyframeParameters Parameters { get; set; }
-    }
-}
