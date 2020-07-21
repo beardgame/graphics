feffbe95b87b19104463ea75e08eaccecf584af0
amulware <amul@amulware.net>
2020-07-21 23:12:25 +0100

    🆙 Update example

--- examples/Basics/GameWindow.cs
+++ examples/Basics/GameWindow.cs
@@ -9,9 +9,13 @@ namespace amulware.Graphics.Examples.Basics
 {
     sealed class GameWindow : Window
     {
-        private IndexedSurface<PrimitiveVertexData> surface;
-        private Matrix4Uniform viewMatrix;
-        private Matrix4Uniform projectionMatrix;
+        // Matrix4Uniform (and similarly other _Uniform classes) represent an input for the shaders.
+        // When a uniform is linked to a renderer, it will automatically assign the values of the uniform to the
+        // respective uniform in the vertex shader (see geometry.vs for the corresponding inputs).
+        private readonly Matrix4Uniform viewMatrix = new Matrix4Uniform("view", Matrix4.Identity);
+        private readonly Matrix4Uniform projectionMatrix = new Matrix4Uniform("projection", Matrix4.Identity);
+
+        private Renderer? renderer;
 
         public GameWindow()
             : base(
@@ -27,25 +31,24 @@ namespace amulware.Graphics.Examples.Basics
         {
         }
 
-
         protected override void OnLoad()
         {
-            // A surface is a vertex buffer wrapper.
-            // We use an indexed one, which is the most commonly used in amulware.Graphics.
-            surface = new IndexedSurface<PrimitiveVertexData>();
+            // ...
+            var buffer = new Buffer<ColorVertexData>();
+            addTriangle(buffer);
 
-            // Matrix4Uniform (and similarly other _Uniform classes) represent an input for the shaders.
-            // When a uniform is linked to a surface, it will automatically assign the values of the uniform to the
-            // respective uniform in the vertex shader (see geometry.vs for the corresponding inputs).
-            viewMatrix = new Matrix4Uniform("view");
-            projectionMatrix = new Matrix4Uniform("projection");
-            surface.AddSettings(viewMatrix, projectionMatrix);
+            // ...
+            var renderable = Renderable.ForVertices(buffer, PrimitiveType.Triangles);
 
-            // The shader program contains the vertex and fragment shaders used to render the vertices from the surface.
-            surface.SetShaderProgram(ShaderProgram.FromFiles("geometry.vs", "geometry.fs"));
+            // The shader program contains the vertex and fragment shaders. It is assigned to a renderer.
+            var shaderProgram = ShaderProgram.FromShaders(
+                ShaderFactory.Geometry.FromFile("geometry.vs"), ShaderFactory.Fragment.FromFile("geometry.fs"));
+
+            // ...
+            renderer = Renderer.From(renderable, shaderProgram, viewMatrix, projectionMatrix);
 
             // Initialize a reasonable view matrix.
-            viewMatrix.Matrix = Matrix4.LookAt(new Vector3(0, 0, -2), Vector3.Zero, Vector3.UnitY);
+            viewMatrix.Value = Matrix4.LookAt(new Vector3(0, 0, -2), Vector3.Zero, Vector3.UnitY);
         }
 
         protected override void OnResize(ResizeEventArgs e)
@@ -53,7 +56,7 @@ namespace amulware.Graphics.Examples.Basics
             // OnResize is also called when the window is initially opened, so it is safe to initialize matrices here.
 
             // Use the simplest possible projection matrix: an orthographic projection.
-            projectionMatrix.Matrix = Matrix4.CreateOrthographic(e.Width, e.Height, .1f, 100f);
+            projectionMatrix!.Value = Matrix4.CreateOrthographic(e.Width, e.Height, .1f, 100f);
         }
 
         protected override void OnUpdate(UpdateEventArgs e)
@@ -64,12 +67,8 @@ namespace amulware.Graphics.Examples.Basics
         {
             prepareForFrame();
 
-            // The actual drawing code. This uploads all the necessary geometry to the graphics card.
-            drawTriangle();
-
-            // Renders the geometry to the buffer. By default, this will also clear the contents of the surface to
-            // prepare it for next frame.
-            surface.Render();
+            // Renders the geometry to the current render target.
+            renderer!.Render();
 
             // Since we do double-buffered rendering, we swap the two buffers when we're done rendering our current
             // frame.
@@ -78,23 +77,26 @@ namespace amulware.Graphics.Examples.Basics
 
         private void prepareForFrame()
         {
-            // Clear the entire buffer, using black as clear color.
+            // Clear the entire render target, using black as clear color.
             var argb = Color.Black;
             GL.ClearColor(argb.R / 255f, argb.G / 255f, argb.B / 255f, 1);
             GL.Clear(ClearBufferMask.ColorBufferBit);
         }
 
-        private void drawTriangle()
+        private void addTriangle(Buffer<ColorVertexData> buffer)
         {
-            // We draw a triangle by specifying the three corners and using the AddTriangle helper on the surface to
-            // take care of adding the right vertices and indices to the right buffers.
-            var v0 = new PrimitiveVertexData(-200, -100f, 0, Color.Red);
-            var v1 = new PrimitiveVertexData(200, -100f, 0, Color.Green);
-            var v2 = new PrimitiveVertexData(0, 200, 0, Color.Blue);
+            // You would often use a mesh builder or other helper class to draw shapes, such as rectangles, lines, and
+            // even fonts or textures. However, for this example we add vertices to the buffer directly.
+            using var target = buffer.Bind();
 
-            // Usually you would use a geometry class to draw shapes, such as rectangles, lines, and even fonts or
-            // textures. However, for this example we add vertices to the surface directly.
-            surface.AddTriangle(v0, v1, v2);
+            // We draw a triangle by specifying the three corners and using the AddTriangle helper on the mesh builder
+            // to take care of adding the right vertices and indices to the right buffers.
+            target.Upload(new []
+            {
+                new ColorVertexData(-200, -100f, 0, Color.Red),
+                new ColorVertexData(200, -100f, 0, Color.Green),
+                new ColorVertexData(0, 200, 0, Color.Blue)
+            });
         }
     }
 }
