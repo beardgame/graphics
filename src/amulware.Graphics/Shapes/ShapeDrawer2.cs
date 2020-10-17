6cf6822dd46e77f370376d94484f091edfe5da65
amulware <amul@amulware.net>
2020-10-17 13:32:26 +0100

    :straight_ruler: Create IShapeDrawer2 interface

--- src/amulware.Graphics/Shapes/ShapeDrawer2.cs
+++ src/amulware.Graphics/Shapes/ShapeDrawer2.cs
@@ -5,7 +5,7 @@ using OpenToolkit.Mathematics;
 
 namespace amulware.Graphics.Shapes
 {
-    public sealed class ShapeDrawer2<TVertex, TVertexParameters>
+    public sealed class ShapeDrawer2<TVertex, TVertexParameters> : IShapeDrawer2<TVertexParameters>
         where TVertex : struct, IVertexData
     {
         public delegate TVertex CreateShapeVertex(Vector3 xyz, TVertexParameters parameters);
@@ -20,21 +20,6 @@ namespace amulware.Graphics.Shapes
             this.createShapeVertex = createShapeVertex;
         }
 
-        public void FillRectangle(Vector2 xy, Vector2 wh, TVertexParameters parameters)
-        {
-            FillRectangle(xy.X, xy.Y, 0, wh.X, wh.Y, parameters);
-        }
-
-        public void FillRectangle(Vector3 xyz, Vector2 wh, TVertexParameters parameters)
-        {
-            FillRectangle(xyz.X, xyz.Y, xyz.Z, wh.X, wh.Y, parameters);
-        }
-
-        public void FillRectangle(float x, float y, float w, float h, TVertexParameters parameters)
-        {
-            FillRectangle(x, y, 0, w, h, parameters);
-        }
-
         public void FillRectangle(float x, float y, float z, float w, float h, TVertexParameters parameters)
         {
             meshBuilder.AddQuad(
@@ -45,20 +30,8 @@ namespace amulware.Graphics.Shapes
             );
         }
 
-        public void DrawRectangle(Vector2 xy, Vector2 wh, float lineWidth, TVertexParameters parameters)
-        {
-            DrawRectangle(xy.X, xy.Y, 0, wh.X, wh.Y, lineWidth, parameters);
-        }
-        public void DrawRectangle(Vector3 xyz, Vector2 wh, float lineWidth, TVertexParameters parameters)
-        {
-            DrawRectangle(xyz.X, xyz.Y, xyz.Z, wh.X, wh.Y, lineWidth, parameters);
-        }
-        public void DrawRectangle(float x, float y, float w, float h, float lineWidth, TVertexParameters parameters)
-        {
-            DrawRectangle(x, y, 0, w, h, lineWidth, parameters);
-        }
-
-        public void DrawRectangle(float x, float y, float z, float w, float h, float lineWidth, TVertexParameters parameters)
+        public void DrawRectangle(
+            float x, float y, float z, float w, float h, float lineWidth, TVertexParameters parameters)
         {
             meshBuilder.Add(8, 24, out var vertices, out var indices, out var indexOffset);
 
@@ -87,100 +60,9 @@ namespace amulware.Graphics.Shapes
             }
         }
 
-        public void FillCircle(float x, float y, float r, TVertexParameters parameters, int edges = 32)
-        {
-            fillOval(x, y, 0, r, r, parameters, edges);
-        }
-
-        public void FillCircle(Vector2 xy, float r, TVertexParameters parameters, int edges = 32)
-        {
-            fillOval(xy.X, xy.Y, 0, r, r, parameters, edges);
-        }
-
-        public void FillCircle(float x, float y, float z, float r, TVertexParameters parameters, int edges = 32)
-        {
-            fillOval(x, y, z, r, r, parameters, edges);
-        }
-
-        public void FillCircle(Vector3 xyz, float r, TVertexParameters parameters, int edges = 32)
-        {
-            fillOval(xyz.X, xyz.Y, xyz.Z, r, r, parameters, edges);
-        }
-
-        public void DrawCircle(float x, float y, float r, float lineWidth, TVertexParameters parameters, int edges = 32)
-        {
-            drawOval(x, y, 0, r, r, lineWidth, parameters, edges);
-        }
-
-        public void DrawCircle(Vector2 xy, float r, float lineWidth, TVertexParameters parameters, int edges = 32)
-        {
-            drawOval(xy.X, xy.Y, 0, r, r, lineWidth, parameters, edges);
-        }
-
-        public void DrawCircle(float x, float y, float z, float r, float lineWidth, TVertexParameters parameters, int edges = 32)
-        {
-            drawOval(x, y, z, r, r, lineWidth, parameters, edges);
-        }
-
-        public void DrawCircle(Vector3 xyz, float r, float lineWidth, TVertexParameters parameters, int edges = 32)
-        {
-            drawOval(xyz.X, xyz.Y, xyz.Z, r, r, lineWidth, parameters, edges);
-        }
-
-        public void FillOval(Vector2 xy, Vector2 wh, TVertexParameters parameters, int edges = 32)
-        {
-            wh *= 0.5f;
-            fillOval(xy.X + wh.X, xy.Y + wh.Y, 0, wh.X, wh.Y, parameters, edges);
-        }
-
-        public void FillOval(Vector3 xyz, Vector2 wh, TVertexParameters parameters, int edges = 32)
-        {
-            wh *= 0.5f;
-            fillOval(xyz.X + wh.X, xyz.Y + wh.Y, xyz.Z, wh.X, wh.Y, parameters, edges);
-        }
-
-        public void FillOval(float x, float y, float w, float h, TVertexParameters parameters, int edges = 32)
-        {
-            w *= 0.5f;
-            h *= 0.5f;
-            fillOval(x + w, y + h, 0, w, h, parameters, edges);
-        }
-
-        public void FillOval(float x, float y, float z, float w, float h, TVertexParameters parameters, int edges = 32)
-        {
-            w *= 0.5f;
-            h *= 0.5f;
-            fillOval(x + w, y + h, z, w, h, parameters, edges);
-        }
-
-        public void DrawOval(Vector2 xy, Vector2 wh, float lineWidth, TVertexParameters parameters, int edges = 32)
-        {
-            wh *= 0.5f;
-            drawOval(xy.X + wh.X, xy.Y + wh.Y, 0, wh.X, wh.Y, lineWidth, parameters, edges);
-        }
-
-        public void DrawOval(Vector3 xyz, Vector2 wh, float lineWidth, TVertexParameters parameters, int edges = 32)
-        {
-            wh *= 0.5f;
-            drawOval(xyz.X + wh.X, xyz.Y + wh.Y, xyz.Z, wh.X, wh.Y, lineWidth, parameters, edges);
-        }
-
-        public void DrawOval(float x, float y, float w, float h, float lineWidth, TVertexParameters parameters, int edges = 32)
-        {
-            w *= 0.5f;
-            h *= 0.5f;
-            drawOval(x + w, y + h, 0, w, h, lineWidth, parameters, edges);
-        }
-
-        public void DrawOval(float x, float y, float z, float w, float h, float lineWidth, TVertexParameters parameters, int edges = 32)
-        {
-            w *= 0.5f;
-            h *= 0.5f;
-            drawOval(x + w, y + h, z, w, h, lineWidth, parameters, edges);
-        }
-
-        private void fillOval(
-            float centerX, float centerY, float centerZ, float halfWidth, float halfHeight, TVertexParameters parameters, int edges)
+        public void FillOval(
+            float centerX, float centerY, float centerZ, float radiusX, float radiusY,
+            TVertexParameters parameters, int edges)
         {
             if (edges < 3)
                 throw new ArgumentOutOfRangeException(nameof(edges), "Must draw at least three edges.");
@@ -192,14 +74,14 @@ namespace amulware.Graphics.Shapes
             var xy = new Vector2(0, -1);
 
             vertices[0] = createShapeVertex(new Vector3(
-                centerX + xy.X * halfWidth, centerY + xy.Y * halfHeight, centerZ), parameters);
+                centerX + xy.X * radiusX, centerY + xy.Y * radiusY, centerZ), parameters);
 
             for (var i = 1; i < edges; i++)
             {
                 xy = rotation * xy;
 
                 vertices[i] = createShapeVertex(new Vector3(
-                    centerX + xy.X * halfWidth, centerY + xy.Y * halfHeight, centerZ), parameters);
+                    centerX + xy.X * radiusX, centerY + xy.Y * radiusY, centerZ), parameters);
             }
 
             for (var i = 0; i < edges - 2; i++)
@@ -211,8 +93,8 @@ namespace amulware.Graphics.Shapes
             }
         }
 
-        private void drawOval(
-            float centerX, float centerY, float centerZ, float halfWidth, float halfHeight,
+        public void DrawOval(
+            float centerX, float centerY, float centerZ, float radiusX, float radiusY,
             float lineWidth, TVertexParameters parameters, int edges)
         {
             if (edges < 3)
@@ -224,11 +106,11 @@ namespace amulware.Graphics.Shapes
 
             var xy = new Vector2(0, -1);
 
-            var innerW = halfWidth - lineWidth;
-            var innerH = halfHeight - lineWidth;
+            var innerW = radiusX - lineWidth;
+            var innerH = radiusY - lineWidth;
 
             vertices[0] = createShapeVertex(new Vector3(
-                centerX + xy.X * halfWidth, centerY + xy.Y * halfHeight, centerZ), parameters);
+                centerX + xy.X * radiusX, centerY + xy.Y * radiusY, centerZ), parameters);
 
             vertices[1] = createShapeVertex(new Vector3(
                 centerX + xy.X * innerW, centerY + xy.Y * innerH, centerZ), parameters);
@@ -238,7 +120,7 @@ namespace amulware.Graphics.Shapes
                 xy = rotation * xy;
 
                 vertices[2 * i] = createShapeVertex(new Vector3(
-                    centerX + xy.X * halfWidth, centerY + xy.Y * halfHeight, centerZ), parameters);
+                    centerX + xy.X * radiusX, centerY + xy.Y * radiusY, centerZ), parameters);
 
                 vertices[2 * i + 1] = createShapeVertex(new Vector3(
                     centerX + xy.X * innerW, centerY + xy.Y * innerH, centerZ), parameters);
@@ -268,22 +150,8 @@ namespace amulware.Graphics.Shapes
             indices[lastJ + 5] = (ushort)(indexOffset + 1);
         }
 
-        public void DrawLine(Vector2 xy1, Vector2 xy2, float lineWidth, TVertexParameters parameters)
-        {
-            DrawLine(xy1.X, xy1.Y, 0, xy2.X, xy2.Y, 0, lineWidth, parameters);
-        }
-
-        public void DrawLine(Vector3 xyz1, Vector3 xyz2, float lineWidth, TVertexParameters parameters)
-        {
-            DrawLine(xyz1.X, xyz1.Y, xyz1.Z, xyz2.X, xyz2.Y, xyz2.Z, lineWidth, parameters);
-        }
-
-        public void DrawLine(float x1, float y1, float x2, float y2, float lineWidth, TVertexParameters parameters)
-        {
-            DrawLine(x1, y1, 0, x2, y2, 0, lineWidth, parameters);
-        }
-
-        public void DrawLine(float x1, float y1, float z1, float x2, float y2, float z2, float lineWidth, TVertexParameters parameters)
+        public void DrawLine(
+            float x1, float y1, float z1, float x2, float y2, float z2, float lineWidth, TVertexParameters parameters)
         {
             var vx = x2 - x1;
             var vy = y1 - y2; // switch order for correct normal direction
