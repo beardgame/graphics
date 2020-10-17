2c491ff09f9556a95d0ff1806ad46de532a4f862
amulware <amul@amulware.net>
2020-10-17 12:24:20 +0100

    🔓 Expose TextDrawerWithDefaults better

--- src/amulware.Graphics/Text/ITextDrawer.cs
+++ src/amulware.Graphics/Text/ITextDrawer.cs
@@ -2,16 +2,9 @@ using OpenToolkit.Mathematics;
 
 namespace amulware.Graphics.Text
 {
-    public interface ITextDrawer<TVertexParameters>
+    public interface ITextDrawer<in TVertexParameters>
     {
         void DrawLine(Vector3 xyz, string text, float fontHeight, float alignHorizontal, float alignVertical,
             Vector3 unitRightDP, Vector3 unitDownDP, TVertexParameters parameters);
-
-        TextDrawerWithDefaults<TVertexParameters> WithDefaults(float fontHeight, float alignHorizontal, float alignVertical,
-            Vector3 unitRightDp, Vector3 unitDownDp, TVertexParameters parameters)
-        {
-            return new TextDrawerWithDefaults<TVertexParameters>(
-                this, fontHeight, alignHorizontal, alignVertical, unitRightDp, unitDownDp, parameters);
-        }
     }
 }
