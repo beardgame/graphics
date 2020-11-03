272cdaec32fddea5b42264b379c65885cd691d7f
Tom Rijnbeek <tom@tomrijnbeek.me>
2020-11-03 20:39:40 +0000

    🔡 Use a fallback glyph for fonts (#29)

--- src/amulware.Graphics/Text/Font.cs
+++ src/amulware.Graphics/Text/Font.cs
@@ -7,14 +7,29 @@ namespace amulware.Graphics.Text
     {
         private readonly ImmutableDictionary<char, CharacterInfo> characters;
 
-        public Font(ImmutableDictionary<char, CharacterInfo> characters)
+        public CharacterInfo FallbackCharacter { get; }
+
+        public Font(ImmutableDictionary<char, CharacterInfo> characters, CharacterInfo fallbackCharacter)
         {
             this.characters = characters;
+            FallbackCharacter = fallbackCharacter;
         }
 
         public CharacterInfo GetCharacterInfoFor(char character)
         {
-            return characters[character];
+            return TryGetCharacterInfoFor(character, out var info) ? info : FallbackCharacter;
+        }
+
+        public bool TryGetCharacterInfoFor(char character, out CharacterInfo characterInfo)
+        {
+            if (characters.TryGetValue(character, out var foundValue))
+            {
+                characterInfo = foundValue;
+                return true;
+            }
+
+            characterInfo = default;
+            return false;
         }
 
         public float StringWidth(string text)
