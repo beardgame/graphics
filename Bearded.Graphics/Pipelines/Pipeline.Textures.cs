de857fb79a3df656c3bcadd6a0b361a2564e953e
amulware <amul@amulware.net>
2021-11-04 13:03:56 +0000

    🏷 Add pipeline texture and render target debug labels

--- Bearded.Graphics/Pipelines/Pipeline.Textures.cs
+++ Bearded.Graphics/Pipelines/Pipeline.Textures.cs
@@ -7,6 +7,24 @@ namespace Bearded.Graphics.Pipelines
 {
     public static partial class Pipeline
     {
+
+        public static PipelineRenderTarget RenderTargetWithColors(string label, params PipelineTexture[] textures)
+        {
+            return withLabel(label, RenderTargetWithColors(textures));
+        }
+
+        public static PipelineRenderTarget RenderTargetWithDepthAndColors(
+            string label, PipelineDepthTexture depth, params PipelineTexture[] textures)
+        {
+            return withLabel(label, RenderTargetWithDepthAndColors(depth, textures));
+        }
+
+        private static PipelineRenderTarget withLabel(string label, PipelineRenderTarget target)
+        {
+            GL.ObjectLabel(ObjectLabelIdentifier.Framebuffer, target.Handle, label.Length, label);
+            return target;
+        }
+
         public static PipelineRenderTarget RenderTargetWithColors(params PipelineTexture[] textures)
         {
             return renderTarget(null, textures);
@@ -42,33 +60,45 @@ namespace Bearded.Graphics.Pipelines
         public static PipelineTexture Texture(
             PixelInternalFormat pixelFormat,
             int width = 1, int height = 1,
-            Action<Texture.Target>? setup = null)
+            Action<Texture.Target>? setup = null,
+            string? label = null)
         {
             var texture = Textures.Texture.Empty(width, height, pixelFormat);
 
+            optionalTextureSetup(texture, setup, label);
+
+            return new PipelineTexture(texture);
+        }
+        public static PipelineDepthTexture DepthTexture(
+            PixelInternalFormat pixelFormat,
+            int width = 1, int height = 1,
+            Action<Texture.Target>? setup = null,
+            string? label = null)
+        {
+            var texture = Textures.Texture.Empty(width, height, pixelFormat, PixelFormat.DepthComponent, PixelType.Float);
+
+            optionalTextureSetup(texture, target =>
+            {
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.DepthTextureMode, (int) All.Intensity);
+                setup?.Invoke(target);
+            }, label);
+
+            return new PipelineDepthTexture(texture);
+        }
+
+        private static void optionalTextureSetup(
+            Texture texture, Action<Texture.Target>? setup, string? label)
+        {
             if (setup != null)
             {
                 using var target = texture.Bind();
                 setup(target);
             }
 
-            return new PipelineTexture(texture);
-        }
-
-        public static PipelineDepthTexture DepthTexture(
-            PixelInternalFormat pixelFormat,
-            int width = 1, int height = 1,
-            Action<Texture.Target>? setup = null)
-        {
-            var texture = Textures.Texture.Empty(width, height, pixelFormat, PixelFormat.DepthComponent, PixelType.Float);
-
-            using (var target = texture.Bind())
+            if (label != null)
             {
-                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.DepthTextureMode, (int) All.Intensity);
-                setup?.Invoke(target);
+                GL.ObjectLabel(ObjectLabelIdentifier.Texture, texture.Handle, label.Length, label);
             }
-
-            return new PipelineDepthTexture(texture);
         }
     }
 }
