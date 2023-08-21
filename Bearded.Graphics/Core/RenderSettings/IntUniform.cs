08fd0893379a175c3b9591776724a4f250210af3
Paul C. Scharf <paul@paulscharf.com>
2023-08-21 22:37:24 +0100

    🔢 Add integer uniform

---
+++ Bearded.Graphics/Core/RenderSettings/IntUniform.cs
@@ -0,0 +1,19 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace Bearded.Graphics.RenderSettings;
+
+public sealed class IntUniform : Uniform<int>
+{
+    public IntUniform(string name) : base(name, 0)
+    {
+    }
+
+    public IntUniform(string name, int value) : base(name, value)
+    {
+    }
+
+    protected override void SetAtLocation(int location)
+    {
+        GL.Uniform1(location, Value);
+    }
+}
