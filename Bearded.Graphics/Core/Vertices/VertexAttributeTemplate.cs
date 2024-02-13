3ff71f96850e4dddbb81caf64ee1a33555a6818a
Paul C. Scharf <paul@paulscharf.com>
2024-02-13 11:03:49 +0000

    🏗️ Rework vertex data (#73)
    * 🆙 Upgrade to .net8
    
    * 🏗️ Rework vertex data
    
    - make IVertexData.VertexAttributes static and immutable
    - support double and integer vertex attributes
    - remove automatic normalisation for integer types
    - fix attribute size being incorrectly calculated based on attribute pointer type, instead of source data
    - make vertex attributes and templates structs and improve method signatures to reduce allocations
    - move all parameter validation to helper class
    
    * ⏪ Undo separately hardcoded vertex attribute byte sizes
    
    * 🆙 Fix example C# versions

--- Bearded.Graphics/Core/Vertices/VertexAttributeTemplate.cs
+++ Bearded.Graphics/Core/Vertices/VertexAttributeTemplate.cs
@@ -1,45 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using OpenTK.Graphics.OpenGL;
 
-namespace Bearded.Graphics.Vertices
+namespace Bearded.Graphics.Vertices;
+
+public readonly struct VertexAttributeTemplate(
+    string name,
+    int size,
+    int bytes,
+    VertexAttribPointerType type,
+    VertexAttributeFormat format)
 {
-    public sealed class VertexAttributeTemplate
-    {
-        private static readonly ImmutableDictionary<VertexAttribPointerType, int> attributeByteSizes
-            = ImmutableDictionary.CreateRange(new Dictionary<VertexAttribPointerType, int>
-            {
-                { VertexAttribPointerType.Byte, 1 },
-                { VertexAttribPointerType.UnsignedByte, 1 },
-                { VertexAttribPointerType.Short, 2 },
-                { VertexAttribPointerType.UnsignedShort, 2 },
-                { VertexAttribPointerType.HalfFloat, 2 },
-                { VertexAttribPointerType.Int, 4 },
-                { VertexAttribPointerType.UnsignedInt, 4 },
-                { VertexAttribPointerType.Float, 4 },
-                { VertexAttribPointerType.Double, 8 },
-            });
-
-        private readonly string name;
-        private readonly int size;
-        private readonly VertexAttribPointerType type;
-        private readonly bool normalize;
-
-        public int Bytes { get; }
-
-        internal VertexAttributeTemplate(string name, int size, VertexAttribPointerType type, bool normalize)
-        {
-            if (!attributeByteSizes.TryGetValue(type, out var bytes))
-                throw new ArgumentException($"Unknown VertexAttribPointerType: {type}");
-
-            Bytes = bytes * size;
-            this.name = name;
-            this.size = size;
-            this.type = type;
-            this.normalize = normalize;
-        }
-
-        public VertexAttribute ToAttribute(int offset, int stride) => new(name, size, type, stride, offset, normalize);
-    }
+    public int Bytes => bytes;
+    public VertexAttribute ToAttribute(int offset, int stride) => new(name, size, type, stride, offset, format);
 }
--- Bearded.Graphics/Core/Vertices/VertexData.cs
+++ Bearded.Graphics/Core/Vertices/VertexData.cs
@@ -1,147 +1,127 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Bearded.Graphics.Shading;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
+using Half = OpenTK.Mathematics.Half;
 
-namespace Bearded.Graphics.Vertices
+namespace Bearded.Graphics.Vertices;
+
+using PointerType = VertexAttribPointerType;
+using Format = VertexAttributeFormat;
+using Defaults = (VertexAttribPointerType Type, int Count, VertexAttributeFormat DefaultFormat);
+
+public static class VertexData
 {
-    /// <summary>
-    /// This class contains helper types and methods to easily create vertex attribute layouts.
-    /// </summary>
-    public static class VertexData
+    public static void SetAttributes<TVertex>(ShaderProgram program)
+        where TVertex : struct, IVertexData
+        => SetAttributes(TVertex.VertexAttributes, program);
+
+    public static void SetAttributes<TCollection>(TCollection attributes, ShaderProgram program)
+        where TCollection : IEnumerable<VertexAttribute>
     {
-        public static void SetAttributes<TVertex>(ShaderProgram program)
-            where TVertex : struct, IVertexData
+        foreach (var attribute in attributes)
         {
-            SetAttributes(default(TVertex).VertexAttributes, program);
+            attribute.SetAttribute(program);
         }
-
-        public static void SetAttributes(VertexAttribute[] attributes, ShaderProgram program)
-        {
-            foreach (var attribute in attributes)
-            {
-                attribute.SetAttribute(program);
-            }
-        }
-
-        /// <summary>
-        /// Creates a <see cref="VertexAttribute"/> array from a list of attribute templates.
-        /// Offset and stride are calculated automatically, assuming zero padding.
-        /// </summary>
-        /// <param name="attributes">The attribute templates.</param>
-        public static VertexAttribute[] MakeAttributeArray(IList<VertexAttributeTemplate> attributes)
-        {
-            var stride = attributes.Sum(a => a.Bytes);
-            var array = new VertexAttribute[attributes.Count];
-            var offset = 0;
-            for (var i = 0; i < attributes.Count; i++)
-            {
-                var template = attributes[i];
-                array[i] = template.ToAttribute(offset, stride);
-                offset += template.Bytes;
-            }
-            return array;
-        }
-
-        /// <summary>
-        /// Creates a <see cref="VertexAttribute"/> array from a list of attribute templates.
-        /// Offset and stride are calculated automatically, assuming zero padding.
-        /// </summary>
-        /// <param name="attributes">The attribute templates.</param>
-        public static VertexAttribute[] MakeAttributeArray(params VertexAttributeTemplate[] attributes) =>
-            MakeAttributeArray((IList<VertexAttributeTemplate>) attributes);
-
-        /// <summary>
-        /// Creates a <see cref="VertexAttribute"/> array from a list of attribute templates.
-        /// Offset and stride are calculated automatically, assuming zero padding.
-        /// </summary>
-        /// <param name="attributes">The attribute templates.</param>
-        public static VertexAttribute[] MakeAttributeArray(IEnumerable<VertexAttributeTemplate> attributes) =>
-            MakeAttributeArray(attributes.ToList());
-
-        private static readonly ImmutableDictionary<Type, (VertexAttribPointerType Type, int Count, bool DefaultNormalize)> knownTypes
-            = ImmutableDictionary.CreateRange(new Dictionary<Type, (VertexAttribPointerType, int, bool)>
-            {
-                { typeof(float), (VertexAttribPointerType.Float, 1, false) },
-                { typeof(Vector2), (VertexAttribPointerType.Float, 2, false) },
-                { typeof(Vector3), (VertexAttribPointerType.Float, 3, false) },
-                { typeof(Vector4), (VertexAttribPointerType.Float, 4, false) },
-
-                { typeof(OpenTK.Mathematics.Half), (VertexAttribPointerType.HalfFloat, 1, false) },
-                { typeof(Vector2h), (VertexAttribPointerType.HalfFloat, 2, false) },
-                { typeof(Vector3h), (VertexAttribPointerType.HalfFloat, 3, false) },
-                { typeof(Vector4h), (VertexAttribPointerType.HalfFloat, 4, false) },
-
-                { typeof(double), (VertexAttribPointerType.Double, 1, false) },
-                { typeof(Vector2d), (VertexAttribPointerType.HalfFloat, 2, false) },
-                { typeof(Vector3d), (VertexAttribPointerType.HalfFloat, 3, false) },
-                { typeof(Vector4d), (VertexAttribPointerType.HalfFloat, 4, false) },
-
-                { typeof(byte), (VertexAttribPointerType.UnsignedByte, 1, true) },
-                { typeof(sbyte), (VertexAttribPointerType.Byte, 1, true) },
-
-                { typeof(short), (VertexAttribPointerType.Short, 1, false) },
-                { typeof(ushort), (VertexAttribPointerType.UnsignedShort, 1, false) },
-
-                { typeof(int), (VertexAttribPointerType.Int, 1, false) },
-                { typeof(uint), (VertexAttribPointerType.UnsignedInt, 1, false) },
-
-                { typeof(Color), (VertexAttribPointerType.UnsignedByte, 4, true) },
-            });
-
-        /// <summary>
-        /// Creates a vertex attribute template of a given basic type with and a given name.
-        /// </summary>
-        /// <param name="name">The name of the attribute in shader code.</param>
-        /// <param name="normalize">
-        /// Whether to normalize the attribute.
-        /// For null, only attributes of type <see cref="byte"/>, <see cref="sbyte"/>, and <see cref="Color"/> are normalised.
-        /// Default is null.
-        /// </param>
-        /// <typeparam name="T">
-        /// The type the attribute.
-        /// Supported are all signed and unsigned integer types, float, double and <see cref="Half"/>,
-        /// three and four dimensional vectors of all three floating point types, and <see cref="Color"/>.
-        /// </typeparam>
-        /// <exception cref="ArgumentException">The given type is not supported.</exception>
-        public static VertexAttributeTemplate MakeAttributeTemplate<T>(string name, bool? normalize = null) =>
-            MakeAttributeTemplate(name, typeof (T), normalize);
-
-        /// <summary>
-        /// Creates a vertex attribute template of a given basic type with and a given name.
-        /// </summary>
-        /// <param name="name">The name of the attribute in shader code.</param>
-        /// <param name="type">
-        /// The type the attribute.
-        /// Supported are all signed and unsigned integer types, float, double and <see cref="Half"/>,
-        /// three and four dimensional vectors of all three floating point types, and <see cref="Color"/>.
-        /// </param>
-        /// <param name="normalize">
-        /// Whether to normalize the attribute.
-        /// For null, only attributes of type <see cref="byte"/>, <see cref="sbyte"/>, and <see cref="Color"/> are normalised.
-        /// Default is null.
-        /// </param>
-        /// <exception cref="ArgumentException">The given type is not supported.</exception>
-        public static VertexAttributeTemplate MakeAttributeTemplate(string name, Type type, bool? normalize = null)
-        {
-            if(!knownTypes.TryGetValue(type, out var info))
-                throw new ArgumentException($"Unknown type: {type.Name}");
-
-            return MakeAttributeTemplate(name, info.Type, info.Count, normalize ?? info.DefaultNormalize);
-        }
-
-        /// <summary>
-        /// Creates a vertex attribute template of a given basic type with and a given name.
-        /// </summary>
-        /// <param name="name">The name of the attribute in shader code.</param>
-        /// <param name="type">The <see cref="VertexAttribPointerType"/> of the attribute.</param>
-        /// <param name="numberOfType">Number of components of the given type in this attribute.</param>
-        /// <param name="normalize">Whether to normalize the attribute.</param>
-        public static VertexAttributeTemplate MakeAttributeTemplate(
-                string name, VertexAttribPointerType type, int numberOfType, bool normalize = false) =>
-            new(name, numberOfType, type, normalize);
     }
+
+    public static ImmutableArray<VertexAttribute> MakeAttributeArray(IEnumerable<VertexAttributeTemplate> attributes)
+        => MakeAttributeArray(attributes.ToList());
+
+    public static ImmutableArray<VertexAttribute> MakeAttributeArray(params VertexAttributeTemplate[] attributes)
+        => MakeAttributeArray((ICollection<VertexAttributeTemplate>)attributes);
+
+    public static ImmutableArray<VertexAttribute> MakeAttributeArray(ICollection<VertexAttributeTemplate> attributes)
+    {
+        var stride = attributes.Sum(a => a.Bytes);
+        var array = ImmutableArray.CreateBuilder<VertexAttribute>(attributes.Count);
+        var offset = 0;
+        foreach (var template in attributes)
+        {
+            array.Add(template.ToAttribute(offset, stride));
+            offset += template.Bytes;
+        }
+
+        return array.MoveToImmutable();
+    }
+
+    public static VertexAttributeTemplate MakeAttributeTemplate<T>(string name, Format? format = null)
+        => MakeAttributeTemplate(name, typeof(T), format);
+
+    public static VertexAttributeTemplate MakeAttributeTemplate(string name, Type type, Format? format = null)
+    {
+        if (!defaultsForType.TryGetValue(type, out var info))
+            throw new ArgumentException($"Unknown type: {type.Name}");
+
+        var bytes = size(info.Type);
+
+        return MakeAttributeTemplate(name, info.Type, info.Count, info.Count * bytes, format ?? info.DefaultFormat);
+    }
+
+    public static VertexAttributeTemplate MakeAttributeTemplate(
+        string name, PointerType type, int numberOfType, int sizeInBytes, Format format)
+    {
+        if (format == Format.Integer && !isValidIntegerType(type))
+            throw new ArgumentException("Invalid type for integer vertex attribute. Must be an integer.");
+        if (format == Format.Double && type != PointerType.Double)
+            throw new ArgumentException("Invalid type for 64-bit vertex attribute. Must be Double.");
+
+        return new VertexAttributeTemplate(name, numberOfType, sizeInBytes, type, format);
+    }
+
+    private static bool isValidIntegerType(PointerType type)
+        => type is >= PointerType.Byte and <= PointerType.UnsignedInt;
+
+    private static readonly ReadOnlyDictionary<Type, Defaults> defaultsForType
+        = new Dictionary<Type, Defaults>
+        {
+            { typeof(float), (PointerType.Float, 1, Format.Float) },
+            { typeof(Vector2), (PointerType.Float, 2, Format.Float) },
+            { typeof(Vector3), (PointerType.Float, 3, Format.Float) },
+            { typeof(Vector4), (PointerType.Float, 4, Format.Float) },
+
+            { typeof(Half), (PointerType.HalfFloat, 1, Format.Float) },
+            { typeof(Vector2h), (PointerType.HalfFloat, 2, Format.Float) },
+            { typeof(Vector3h), (PointerType.HalfFloat, 3, Format.Float) },
+            { typeof(Vector4h), (PointerType.HalfFloat, 4, Format.Float) },
+
+            { typeof(double), (PointerType.Double, 1, Format.Double) },
+            { typeof(Vector2d), (PointerType.Double, 2, Format.Double) },
+            { typeof(Vector3d), (PointerType.Double, 3, Format.Double) },
+            { typeof(Vector4d), (PointerType.Double, 4, Format.Double) },
+
+            { typeof(byte), (PointerType.UnsignedByte, 1, Format.Integer) },
+            { typeof(sbyte), (PointerType.Byte, 1, Format.Integer) },
+
+            { typeof(short), (PointerType.Short, 1, Format.Integer) },
+            { typeof(ushort), (PointerType.UnsignedShort, 1, Format.Integer) },
+
+            { typeof(int), (PointerType.Int, 1, Format.Integer) },
+            { typeof(uint), (PointerType.UnsignedInt, 1, Format.Integer) },
+
+            { typeof(Color), (PointerType.UnsignedByte, 4, Format.FloatNormalized) },
+        }.AsReadOnly();
+
+    private static int size(PointerType type)
+        => type switch
+        {
+            PointerType.Byte => 1,
+            PointerType.UnsignedByte => 1,
+            PointerType.Short => 2,
+            PointerType.UnsignedShort => 2,
+            PointerType.Int => 4,
+            PointerType.UnsignedInt => 4,
+            PointerType.Float => 4,
+            PointerType.Double => 8,
+            PointerType.HalfFloat => 2,
+            PointerType.Fixed => 4,
+            PointerType.UnsignedInt2101010Rev => 4,
+            PointerType.UnsignedInt10F11F11FRev => 4,
+            PointerType.Int2101010Rev => 4,
+            _ => throw new ArgumentOutOfRangeException($"Invalid {nameof(VertexAttribPointerType)} value: {type}")
+        };
 }
