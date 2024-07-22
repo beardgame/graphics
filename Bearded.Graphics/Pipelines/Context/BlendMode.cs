e491ec1f0b56ec9cbcfcc8ecc92b6c2bc61932b4
Paul C. Scharf <paul@paulscharf.com>
2024-07-22 20:46:08 +0100

    🛠️ Allow custom blend modes in pipeline

--- Bearded.Graphics/Pipelines/Context/BlendMode.cs
+++ Bearded.Graphics/Pipelines/Context/BlendMode.cs
@@ -1,14 +1,19 @@
-namespace Bearded.Graphics.Pipelines.Context
+using OpenTK.Graphics.OpenGL;
+using static OpenTK.Graphics.OpenGL.BlendingFactor;
+using static OpenTK.Graphics.OpenGL.BlendEquationMode;
+
+namespace Bearded.Graphics.Pipelines.Context;
+
+public readonly record struct BlendMode(
+    BlendingFactor Source, BlendingFactor Destination, BlendEquationMode Function)
 {
-    public enum BlendMode
-    {
-        None = 0,
-        Alpha,
-        Add,
-        Subtract,
-        Multiply,
-        Premultiplied,
-        Min,
-        Max,
-    }
+    public static BlendMode Disable => default;
+
+    public static BlendMode Alpha => new(SrcAlpha, OneMinusSrcAlpha, FuncAdd);
+    public static BlendMode Add => new(SrcAlpha, One, FuncAdd);
+    public static BlendMode Subtract => new(SrcAlpha, One, FuncReverseSubtract);
+    public static BlendMode Multiply => new(Zero, SrcColor, FuncAdd);
+    public static BlendMode Premultiplied => new(One, OneMinusSrcAlpha, FuncAdd);
+    public static BlendMode Min => new(One, One, BlendEquationMode.Min);
+    public static BlendMode Max => new(One, One, BlendEquationMode.Max);
 }
